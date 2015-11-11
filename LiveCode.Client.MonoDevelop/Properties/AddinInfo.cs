67a5d6f2016889d34dcf705c1314e482f297f8ea
Frank A. Krueger <fak@praeclarum.org>
2015-11-10 20:51:15 -0800

    Release 1.2.1

--- LiveCode.Client.MonoDevelop/Properties/AddinInfo.cs
+++ LiveCode.Client.MonoDevelop/Properties/AddinInfo.cs
@@ -5,7 +5,7 @@ using Mono.Addins.Description;
 [assembly:Addin (
 	"LiveCode.Client.MonoDevelop", 
 	Namespace = "LiveCode.Client.MonoDevelop",
-	Version = "1.2"
+	Version = "1.2.1"
 )]
 
 [assembly:AddinName ("LiveCode")]
