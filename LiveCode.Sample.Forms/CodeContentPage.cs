38336faa18e987b0c25f82f8a0b0f835a30cd3a5
Frank A. Krueger <fak@praeclarum.org>
2015-11-29 18:38:57 -0800

    Render Xamarin Forms pages

--- LiveCode.Sample.Forms/CodeContentPage.cs
+++ LiveCode.Sample.Forms/CodeContentPage.cs
@@ -8,11 +8,117 @@ namespace LiveCode.Sample.Forms
 	{
 		public CodeContentPage ()
 		{
+			
 			Content = new StackLayout { 
 				Children = {
-					new Label { Text = "Hello ContentPage" }
-				}
+					new Label { Text = "ContentPage", FontSize = 105, }
+				},
+				VerticalOptions = LayoutOptions.Center,
 			};
+		}
+	}
+
+	public class NamedColorPage : ContentPage
+	{
+		public NamedColorPage (bool b)
+		{
+
+			Content = new StackLayout { 
+				Children = {
+					new Label { Text = b.ToString (), FontSize = 105, }
+				},
+				VerticalOptions = LayoutOptions.Center,
+			};
+		}
+	}
+
+	public class NamedColor {
+		public NamedColor (string s, Color c)
+		{
+			
+		}
+	}
+
+	public class MasterDetailPageDemoPage :  MasterDetailPage
+	{
+		public MasterDetailPageDemoPage()
+		{
+			Label header = new Label
+			{
+				Text = "MasterDetailPage",
+				FontSize = Device.GetNamedSize (NamedSize.Large, typeof(Label)),
+				HorizontalOptions = LayoutOptions.Center,
+			};
+
+			// Assemble an array of NamedColor objects.
+			NamedColor[] namedColors = 
+			{
+				new NamedColor("Aqua", Color.Aqua),
+				new NamedColor("Black", Color.Black),
+				new NamedColor("Blue", Color.Blue),
+				new NamedColor("Fuchsia", Color.Fuchsia),
+				new NamedColor("Gray", Color.Gray),
+				new NamedColor("Green", Color.Green),
+				new NamedColor("Lime", Color.Lime),
+				new NamedColor("Maroon", Color.Maroon),
+				new NamedColor("Navy", Color.Navy),
+				new NamedColor("Olive", Color.Olive),
+				new NamedColor("Purple", Color.Purple),
+				new NamedColor("Red", Color.Red),
+				new NamedColor("Silver", Color.Silver),
+				new NamedColor("Teal", Color.Teal),
+				new NamedColor("White", Color.White),
+				new NamedColor("Yellow", Color.Yellow)
+			};
+
+			// Create ListView for the master page.
+			ListView listView = new ListView
+			{
+				ItemsSource = namedColors
+			};
+
+			// Create the master page with the ListView.
+			this.Master = new ContentPage
+			{
+				Title = header.Text,
+				Content = new StackLayout
+				{
+					Children = 
+					{
+						header, 
+						listView
+					}
+					}
+			};
+
+			// Create the detail page using NamedColorPage and wrap it in a
+			// navigation page to provide a NavigationBar and Toggle button
+			this.Detail = new NavigationPage(new NamedColorPage(true));
+
+			// For Windows Phone, provide a way to get back to the master page.
+			if (Device.OS == TargetPlatform.WinPhone)
+			{
+				(this.Detail as ContentPage).Content.GestureRecognizers.Add(
+					new TapGestureRecognizer((view) =>
+						{
+							this.IsPresented = true;
+						}));
+			}
+
+			// Define a selected handler for the ListView.
+			listView.ItemSelected += (sender, args) =>
+			{
+				// Set the BindingContext of the detail page.
+				this.Detail.BindingContext = args.SelectedItem;
+
+				// Show the detail page.
+				this.IsPresented = false;
+			};
+
+			// Initialize the ListView selection.
+			listView.SelectedItem = namedColors[0];
+
+
 		}
 	}
 }
--- LiveCode.Server.iOS/Visualizer.iOS.cs
+++ LiveCode.Server.iOS/Visualizer.iOS.cs
@@ -37,29 +37,32 @@ namespace LiveCode.Server
 			});
 		}
 
-		UIViewController GetViewer (EvalResult resp)
+		Tuple<UIViewController, bool> GetViewer (EvalResult resp)
 		{
 			var vc = resp.Result as UIViewController;
-
 			if (vc != null)
-				return vc;
+				return Tuple.Create (vc, true);
+			
+			var sv = GetSpecialView (resp.Result);
 
+			vc = sv as UIViewController;
+			if (vc != null)
+				return Tuple.Create (vc, false);
 
-			var v = GetSpecialView (resp.Result);
-
+			var v = sv as UIView;
 			if (v != null) {
 				vc = new UIViewController ();
 				vc.View = v;
+				return Tuple.Create (vc, false);
 			}
-			else {
-				vc = new ObjectInspector (resp.Result);
-			}
-			return vc;
+
+			vc = new ObjectInspector (resp.Result);
+			return Tuple.Create (vc, true);
 		}
 
 		UINavigationController presentedNav = null;
 
-		async Task ShowViewerAsync (UIViewController vc)
+		async Task ShowViewerAsync (Tuple<UIViewController, bool> vcNC)
 		{
 			var window = UIApplication.SharedApplication.KeyWindow;
 			if (window == null)
@@ -68,20 +71,23 @@ namespace LiveCode.Server
 			if (rootVC == null)
 				return;
 
-			var doneButton = new UIBarButtonItem (
-				UIBarButtonSystemItem.Done,
-				(_, __) => rootVC.DismissViewController (true, null));
-			vc.NavigationItem.RightBarButtonItems =
-				new[]{ doneButton }.
-				Concat (vc.NavigationItem.RightBarButtonItems ?? new UIBarButtonItem[0]).
-				ToArray ();
+			var vc = vcNC.Item1;
+			var shouldBeInNav = CanBeInNav (vc) && vcNC.Item2;
 
-			var canBeInNav = CanBeInNav (vc);
+			if (shouldBeInNav) {
+				var doneButton = new UIBarButtonItem (
+					                UIBarButtonSystemItem.Done,
+					                (_, __) => rootVC.DismissViewController (true, null));
+				vc.NavigationItem.RightBarButtonItems =
+					new[]{ doneButton }.
+					Concat (vc.NavigationItem.RightBarButtonItems ?? new UIBarButtonItem[0]).
+					ToArray ();
+			}
 
 			//
 			// Try to just swap out the root VC if we've already presented
 			//
-			if (canBeInNav && presentedNav != null && rootVC.PresentedViewController == presentedNav) {
+			if (shouldBeInNav && presentedNav != null && rootVC.PresentedViewController == presentedNav) {
 				presentedNav.ViewControllers = new[] { vc };
 				return;
 			}
@@ -89,7 +95,7 @@ namespace LiveCode.Server
 			//
 			// Else, present a new nav VC
 			//
-			var nc = canBeInNav ? new UINavigationController (vc) : null;
+			var nc = shouldBeInNav ? new UINavigationController (vc) : null;
 
 			var animate = true;
 
@@ -108,7 +114,7 @@ namespace LiveCode.Server
 			return !(vc is UISplitViewController) && !(vc is UINavigationController);
 		}
 
-		UIView GetSpecialView (object obj)
+		object GetSpecialView (object obj)
 		{
 			if (obj == null)
 				return null;
@@ -121,7 +127,7 @@ namespace LiveCode.Server
 			}
 		}
 
-		delegate UIView TypeVisualizer (object value);
+		delegate object TypeVisualizer (object value);
 
 		TypeVisualizer FindVisualizer (Type type)
 		{
@@ -146,7 +152,7 @@ namespace LiveCode.Server
 				{ typeof(UITableViewCell).FullName, o => GetView ((UITableViewCell)o) },
 				{ typeof(UICollectionViewCell).FullName, o => GetView ((UICollectionViewCell)o) },
 				{ typeof(UIColor).FullName, o => GetView ((UIColor)o) },
-				{ "Xamarin.Forms.ContentPage", GetFormsContentPage },
+				{ "Xamarin.Forms.Page", GetFormsPage },
 			};
 		}
 		Dictionary<string, TypeVisualizer> typeVisualizers = new Dictionary<string, TypeVisualizer> ();
@@ -241,17 +247,24 @@ namespace LiveCode.Server
 			}
 		}
 
-		UIView GetFormsContentPage (object pageObj)
+		UIViewController GetFormsPage (object pageObj)
 		{
 			var asms = AppDomain.CurrentDomain.GetAssemblies ();
-			var platasm = asms.FirstOrDefault (x => x.GetName ().Name == "Xamarin.Forms.Platform.iOS");
-			var pagex = platasm.GetType ("Xamarin.Forms.PageExtensions");
+			var xamasm = asms.First (x => x.GetName ().Name == "Xamarin.Forms.Core");
+			var platasm = asms.First (x => x.GetName ().Name == "Xamarin.Forms.Platform.iOS");
 
+			// Wrap it in a NavigationPage cause I think it's needed?
+//			var navpage = xamasm.GetType ("Xamarin.Forms.NavigationPage");
+//			var navPageObj = Activator.CreateInstance (navpage, new object[]{ pageObj });
+
+			// Create the VC
+			var pagex = platasm.GetType ("Xamarin.Forms.PageExtensions");
 			var cvc = pagex.GetMethod ("CreateViewController");
 
+//			var nc = (UIViewController)cvc.Invoke (null, new[]{ navPageObj });
 			var vc = (UIViewController)cvc.Invoke (null, new[]{ pageObj });
 
-			return vc.View;
+			return vc;
 		}
 	}
 }
