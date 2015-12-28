27327034b980770177ecf569fa8369eb47ba27bd
Frank A. Krueger <fak@praeclarum.org>
2015-12-28 12:46:05 -0600

    Add buttons to pad

--- Continuous.Client.VisualStudio/MainPadControl.xaml.cs
+++ Continuous.Client.VisualStudio/MainPadControl.xaml.cs
@@ -1,36 +1,35 @@
-﻿//------------------------------------------------------------------------------
-// <copyright file="ContinuousWindowControl.xaml.cs" company="Company">
-//     Copyright (c) Company.  All rights reserved.
-// </copyright>
-//------------------------------------------------------------------------------
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
 
 namespace Continuous.Client.VisualStudio
 {
-    using System.Diagnostics.CodeAnalysis;
-    using System.Windows;
-    using System.Windows.Controls;
-
-    /// <summary>
-    /// Interaction logic for ContinuousWindowControl.
-    /// </summary>
     public partial class MainPadControl : UserControl
     {
-        /// <summary>
-        /// Initializes a new instance of the <see cref="MainPadControl"/> class.
-        /// </summary>
         public MainPadControl()
         {
             this.InitializeComponent();
         }
 
-        /// <summary>
-        /// Handles click on the button by displaying a message box.
-        /// </summary>
-        /// <param name="sender">The event sender.</param>
-        /// <param name="e">The event args.</param>
-        [SuppressMessage("Microsoft.Globalization", "CA1300:SpecifyMessageBoxOptions", Justification = "Sample code")]
-        [SuppressMessage("StyleCop.CSharp.NamingRules", "SA1300:ElementMustBeginWithUpperCaseLetter", Justification = "Default event handler naming pattern")]
-        private void button1_Click(object sender, RoutedEventArgs e)
+        void HandleSetType (object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show(
+                string.Format(System.Globalization.CultureInfo.CurrentUICulture, "Invoked '{0}'", this.ToString()),
+                "Continuous Coding");
+        }
+        void HandleRefresh(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show(
+                string.Format(System.Globalization.CultureInfo.CurrentUICulture, "Invoked '{0}'", this.ToString()),
+                "Continuous Coding");
+        }
+        void HandleClearEdits(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show(
+                string.Format(System.Globalization.CultureInfo.CurrentUICulture, "Invoked '{0}'", this.ToString()),
+                "Continuous Coding");
+        }
+        void HandleStop(object sender, RoutedEventArgs e)
         {
             MessageBox.Show(
                 string.Format(System.Globalization.CultureInfo.CurrentUICulture, "Invoked '{0}'", this.ToString()),
