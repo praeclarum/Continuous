79e1c4474fbedf31c3631d4679dd844c9060f5ff
Frank A. Krueger <fak@praeclarum.org>
2015-12-29 12:55:10 -0600

    Working on WinForms

---
+++ Continuous.Server.Net/HttpServer.Net.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Continuous.Server
+{
+    public partial class HttpServer
+    {
+        partial void GrantServerPermission (string url)
+        {
+            var args = string.Format ("http add urlacl url={0} user={1}", url, System.Environment.UserDomainName);
+            var p = new System.Diagnostics.Process ();
+            p.StartInfo.FileName = "netsh";
+            p.StartInfo.Arguments = args;
+            p.StartInfo.Verb = "runas";
+            p.Start ();
+            p.WaitForExit ();
+        }
+    }
+}
---
+++ Continuous.Server.Net/VM.Net.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Mono.CSharp;
+
+namespace Continuous.Server
+{
+    public partial class VM
+    {
+        partial void PlatformSettings (CompilerSettings settings)
+        {
+        }
+
+        partial void PlatformInit ()
+        {
+            object res;
+            bool hasRes;
+            eval.Evaluate ("using System.Windows.Forms;", out res, out hasRes);
+        }
+    }
+}
+
