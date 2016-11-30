9cf70519d3210d47f78c6f3cd6eae4c948ee7504
Frank A. Krueger <fak@praeclarum.org>
2016-11-29 17:13:01 -0800

    Add hack to get around Xamarin bug

--- Continuous.Sample.iOS/AppDelegate.cs
+++ Continuous.Sample.iOS/AppDelegate.cs
@@ -1,5 +1,11 @@
-﻿using Foundation;
+﻿using System;
+
+using Foundation;
 using UIKit;
+
+using Continuous.Server;
+using System.Threading.Tasks;
+using System.Threading;
 
 namespace Continuous.Sample.iOS
 {
@@ -13,12 +19,30 @@ namespace Continuous.Sample.iOS
 
 		public override bool FinishedLaunching (UIApplication application, NSDictionary launchOptions)
 		{
-			new Continuous.Server.HttpServer ().Run ();
+			new Continuous.Server.HttpServer (vm: new TestVM()).Run ();
 
 			Window = new UIWindow (UIScreen.MainScreen.Bounds);
 			Window.RootViewController = new UIViewController ();
 			Window.MakeKeyAndVisible ();
 			return true;
+		}
+
+		class TestVM : IVM
+		{
+			public EvalResult Eval(EvalRequest code, TaskScheduler mainScheduler, CancellationToken token)
+			{
+				var t = Task.Factory.StartNew(() => {
+					var r = new EvalResult();
+					r.HasResult = true;
+					r.Result = new UILabel
+					{
+						BackgroundColor = UIColor.Red,
+						Text = "Hello at " + DateTime.Now,
+					};
+					return r;
+				}, token, TaskCreationOptions.None, mainScheduler);
+				return t.Result;
+			}
 		}
 
 		public override void OnResignActivation (UIApplication application)
