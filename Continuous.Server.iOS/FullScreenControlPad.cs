210acd8e2ba73d083f5b828085e84803dce1e1e3
Frank A. Krueger <fak@praeclarum.org>
2016-12-05 15:39:47 -0800

    Save and restore button position

--- Continuous.Server.iOS/FullScreenControlPad.cs
+++ Continuous.Server.iOS/FullScreenControlPad.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+
 using CoreGraphics;
+using Foundation;
 using UIKit;
 
 namespace Continuous.Server.iOS
@@ -8,20 +10,53 @@ namespace Continuous.Server.iOS
 	{
 		public event EventHandler Dismissed;
 
+		static readonly CGSize defaultSize = new CGSize (44, 44);
+
 		readonly UIButton dismissButton = UIButton.FromType (UIButtonType.RoundedRect);
 
 		public FullScreenControlPad ()
-			: base(new CGRect(0, 0, 44, 44))
+			: base(new CGRect(new CGPoint (0, 0), defaultSize))
 		{
 			BackgroundColor = UIColor.FromWhiteAlpha (0.0f, 0.5f);
 			ClipsToBounds = true;
 
+			RestoreXY ();
+
 			dismissButton.Frame = Bounds;
 			dismissButton.AutoresizingMask = UIViewAutoresizing.FlexibleDimensions;
 			dismissButton.TintColor = UIColor.White;
-			dismissButton.SetTitle ("Hello", UIControlState.Normal);
+			dismissButton.SetTitle ("Dismiss " + Frame, UIControlState.Normal);
 			dismissButton.TouchUpInside += (sender, e) => Dismissed?.Invoke (this, EventArgs.Empty);
 			AddSubview (dismissButton);
+		}
+
+		static CGRect MoveRectIntoBounds (CGRect ifr)
+		{
+			var fr = ifr;
+
+			var sb = UIApplication.SharedApplication.KeyWindow.Bounds;
+			if (fr.X < 0) fr.X = 0;
+			else if (fr.Right > sb.Width) fr.X = sb.Width - fr.Width;
+			if (fr.Y < 0) fr.Y = 0;
+			else if (fr.Bottom > sb.Height) fr.Y = sb.Height - fr.Height;
+
+			return fr;
+		}
+
+		void RestoreXY ()
+		{
+			var defs = NSUserDefaults.StandardUserDefaults;
+			var x = defs.FloatForKey ("Continuous.Server.FullScreenControlPad.X");
+			var y = defs.FloatForKey ("Continuous.Server.FullScreenControlPad.Y");
+			Frame = MoveRectIntoBounds (new CGRect (new CGPoint(x, y), defaultSize));
+		}
+
+		void SaveXY ()
+		{
+			var fr = Frame;
+			var defs = NSUserDefaults.StandardUserDefaults;
+			defs.SetFloat ((float)fr.X, "Continuous.Server.FullScreenControlPad.X");
+			defs.SetFloat ((float)fr.Y, "Continuous.Server.FullScreenControlPad.Y");
 		}
 	}
 }
