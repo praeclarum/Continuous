79c863885ce875d13c29e05e7043f919a4171c95
Frank A. Krueger <fak@praeclarum.org>
2016-12-05 15:24:36 -0800

    Start work on full screen dismisser

---
+++ Continuous.Server.iOS/FullScreenControlPad.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using CoreGraphics;
+using UIKit;
+
+namespace Continuous.Server.iOS
+{
+	public class FullScreenControlPad : UIView
+	{
+		public event EventHandler Dismissed;
+
+		readonly UIButton dismissButton = UIButton.FromType (UIButtonType.RoundedRect);
+
+		public FullScreenControlPad ()
+			: base(new CGRect(0, 0, 44, 44))
+		{
+			BackgroundColor = UIColor.FromWhiteAlpha (0.0f, 0.5f);
+			ClipsToBounds = true;
+
+			dismissButton.Frame = Bounds;
+			dismissButton.AutoresizingMask = UIViewAutoresizing.FlexibleDimensions;
+			dismissButton.TintColor = UIColor.White;
+			dismissButton.SetTitle ("Hello", UIControlState.Normal);
+			dismissButton.TouchUpInside += (sender, e) => Dismissed?.Invoke (this, EventArgs.Empty);
+			AddSubview (dismissButton);
+		}
+	}
+}
