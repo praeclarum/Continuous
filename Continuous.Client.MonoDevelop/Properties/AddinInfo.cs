5cf88a0354d6e4287c0f937f58f0dad2ed4576ef
Frank A. Krueger <fak@praeclarum.org>
2016-12-13 13:16:13 -0800

    Release 2.0.5

--- Continuous.Client.MonoDevelop/Properties/AddinInfo.cs
+++ Continuous.Client.MonoDevelop/Properties/AddinInfo.cs
@@ -5,7 +5,7 @@ using Mono.Addins.Description;
 [assembly:Addin (
 	"Continuous.Client.MonoDevelop", 
 	Namespace = "Continuous.Client.MonoDevelop",
-	Version = "2.0.4"
+	Version = "2.0.5"
 )]
 
 [assembly:AddinName ("Continuous Coding")]
